8e198d79f7b320224289d89ceed710dc3e3fd5e8
Vuologist <anthony.vu2142@gmail.com>
2017-12-17 00:34:55 -0800

    Completed robocopy and tested
    Implemented bookmark migration for firefox and chrome
    testing has not been done for bookmarks

--- UserMigration - CopyFiles.bat
+++ UserMigration - CopyFiles.bat
@@ -1,4 +1,4 @@
-@echo off
+@echo on
 REM Anthony Vu 12/16/17
 REM This script will copy specific folders and files from the
 REM user's computer in order to be migrated to a new computer.
@@ -12,14 +12,13 @@ REM Version 2 idea: make sure a new file or folder not apart of
 REM default environment is copied over as well; if on c_admin profile
 REM bark
 
-REM create support files such as log and dependence files
-REM need to create log files
+REM create dependence files wtf am i suppose to create?!?!?
 setlocal
 
 REM setting up environment if first run
 if not exist %~d0\logs (
 	mkdir logs
-	mkdir dependence files
+REM	mkdir "dependence files"
 )
 
 set profileName=%USERNAME%
@@ -29,31 +28,40 @@ REM local computer file locations
 set localComputerPrepath=C:\Users\%USERNAME%
 
 REM location within storage device
-mkdir "%profileName% - %date%"
-mkdir %~d0\logs\%profileName% - %date%
-set profileFolder="%profileName% - %date%"
+mkdir %profileName%-%date%
+mkdir %~d0\logs\%profileName%-%date%
+set profileFolder=%profileName%-%date%
 set flashDrivePrepath=%~d0\%profileFolder%
+set flashDriveLogPrePath=%~d0\logs\%profileName%-%date%
 
 
-robocopy %localComputerPrepath%\Desktop %flashDrivePrepath%\Desktop /MIR /NP /TEE /LOG+:"%~d0\%logs%\%profileName%Desktop"
+robocopy %localComputerPrepath%\Desktop %flashDrivePrepath%\Desktop /MIR /NP /TEE /LOG+:%flashDriveLogPrePath%\DesktopLog.txt
 
-robocopy %localComputerPrepath%\Documents %flashDrivePrepath%\Documents /MIR /NP /TEE /LOG+:"%~d0\%logs%\%profileName%Documents"
+robocopy %localComputerPrepath%\Documents %flashDrivePrepath%\Documents /MIR /NP /TEE /XD "My Music" "My Pictures" "My Videos" /LOG+:%flashDriveLogPrePath%\Documents.txt
 
-robocopy %localComputerPrepath%\Downloads %flashDrivePrepath%\Downloads /MIR /NP /TEE /LOG+:"%~d0\%logs%\%profileName%Downloads"
+robocopy %localComputerPrepath%\Downloads %flashDrivePrepath%\Downloads /MIR /NP /TEE /LOG+:%flashDriveLogPrePath%\Downloads.txt
 
-robocopy %localComputerPrepath%\Favorites %flashDrivePrepath%\Favorites /MIR /NP /TEE /LOG+:"%~d0\%logs%\%profileName%Favorites"
+robocopy %localComputerPrepath%\Favorites %flashDrivePrepath%\Favorites /MIR /NP /TEE /LOG+:%flashDriveLogPrePath%\Favorites.txt
 
-robocopy %localComputerPrepath%\MicrosoftEdgeBackups %flashDrivePrepath%\MicrosoftEdgeBackups /MIR /NP /TEE /LOG+:"%~d0\%logs%\%profileName%MicrosoftEdgeBackups"
+robocopy %localComputerPrepath%\MicrosoftEdgeBackups %flashDrivePrepath%\MicrosoftEdgeBackups /MIR /NP /TEE /LOG+:%flashDriveLogPrePath%\MicrosoftEdgeBackups.txt
 
-robocopy %localComputerPrepath%\Music %flashDrivePrepath%\Music /MIR /NP /TEE /LOG+:"%~d0\%logs%\%profileName%Music"
+robocopy %localComputerPrepath%\Music %flashDrivePrepath%\Music /MIR /NP /TEE /LOG+:%flashDriveLogPrePath%\Music.txt
 
-robocopy %localComputerPrepath%\Pictures %flashDrivePrepath%\Pictures /MIR /NP /TEE /LOG+:"%~d0\%logs%\%profileName%Pictures"
+robocopy %localComputerPrepath%\Pictures %flashDrivePrepath%\Pictures /MIR /NP /TEE /LOG+:%flashDriveLogPrePath%\Pictures.txt
 
-robocopy %localComputerPrepath%\Videos %flashDrivePrepath%\Videos /MIR /NP /TEE /LOG+:"%~d0\%logs%\%profileName%Videos"
+robocopy %localComputerPrepath%\Videos %flashDrivePrepath%\Videos /MIR /NP /TEE /LOG+:%flashDriveLogPrePath%\Videos.txt
 
+set chromeFolder="C:\Users\%USERNAME%\AppData\Local\Google\Chrome\User Data\Default"
+REM check if default folder is same on all computers
+set firefoxFolder="C:\Users\Dev\AppData\Roaming\Mozilla\Firefox\Profiles\s4bm1y53.default"
 
+if exist %chromeFolder% (
+	copy %chromeFolder%\Bookmarks %flashDriveLogPrePath%
+)
 
-
+if exist %firefoxFolder% (
+	copy %firefoxFolder\places.sqlite %flashDriveLogPrePath%
+)
 
 
 
